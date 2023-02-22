6f07530a74cbdd5e553084740dd1560a17f40ad9
JulianMolina99 <julianmolinac99@gmail.com>
2023-02-21 22:28:46 -0500

    Texto modificado

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world with express jm'))
+app.get('/', (req, res) => res.send('<h1>Express con html</h1>'))
 app.listen(3000);
 console.log('Server on port 3000');
